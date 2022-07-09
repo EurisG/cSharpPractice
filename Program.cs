9b35f6f02b358f97bfcbebe79b054d297799362d
EurisG <euris809@gmail.com>
2022-07-09 11:49:47 -0400

    name and age

--- Program.cs
+++ Program.cs
@@ -5,11 +5,24 @@ namespace app
     {
         static void Main(string[] args)
         {
+            // Declaring variables for age and name
+            int age;
+            string name;
+
             Console.WriteLine("Hello, World!");
             Console.WriteLine("What's your name ?");
-            string name = Console.ReadLine();
-            Console.WriteLine("Nice to meet you " + name);
 
+            name = Console.ReadLine();
+            Console.WriteLine("Nice to meet you " + name + " My name is c#");
+            Console.WriteLine("How old are you ?");
+
+            age = Convert.ToInt32( Console.ReadLine() );
+
+            Console.WriteLine("Wow, You are " + age + " Years old");
+
+          
+
+// ReadKey function makes app wait before it closes after its done running
             Console.ReadKey();
         }
     }
