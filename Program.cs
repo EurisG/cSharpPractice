fc3b2ba665b3253833279f2ffadf9e2d55adf396
EurisG <euris809@gmail.com>
2022-07-09 10:58:06 -0400

    First c# repo

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace app
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What's your name ?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Nice to meet you " + name);
+
+            Console.ReadKey();
+        }
+    }
+
+}
+
